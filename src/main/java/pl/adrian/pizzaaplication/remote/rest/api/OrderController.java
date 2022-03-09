02736d4a04c1790e520e1a389c4ee26e724ed555
AdrianTm1 <awojciechowski1@tm1.edu.pl>
2022-03-09 14:52:06 +0100

    Controllers edited

--- src/main/java/pl/adrian/pizzaaplication/remote/rest/api/OrderController.java
+++ src/main/java/pl/adrian/pizzaaplication/remote/rest/api/OrderController.java
@@ -1,4 +1,52 @@
 package pl.adrian.pizzaaplication.remote.rest.api;
 
+
+import org.hibernate.sql.Update;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.adrian.pizzaaplication.domain.model.OrderStatusType;
+import pl.adrian.pizzaaplication.remote.rest.dto.request.AddOrderDto;
+import pl.adrian.pizzaaplication.remote.rest.dto.request.UpdateOrderDto;
+import pl.adrian.pizzaaplication.remote.rest.dto.response.OrderCollectionDto;
+import pl.adrian.pizzaaplication.remote.rest.dto.response.OrderDto;
+import pl.adrian.pizzaaplication.remote.rest.dto.response.OrderStatusDto;
+import pl.adrian.pizzaaplication.remote.rest.dto.response.TokenDto;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RequestMapping(value = "/api/v1/order", produces = APPLICATION_JSON_VALUE)
+@RestController
 public class OrderController {
+
+    @PostMapping
+    public ResponseEntity<TokenDto> addOrder(@RequestBody AddOrderDto addOrderDto) {
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping("/status/{token}")
+    public ResponseEntity<OrderStatusDto> getStatus(@PathVariable("token")String token) {
+        return ResponseEntity.ok(null);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<OrderCollectionDto> getOrders(@RequestParam("status") OrderStatusType orderStatus,
+                                                        @RequestHeader("Access-Token") String token)  {
+        return ResponseEntity.ok(null);
+    }
+
+    @DeleteMapping("/{order-id}")
+    public ResponseEntity<Void>deleteOrder(@PathVariable("order-id") Integer orderId,
+                                                        @RequestHeader("Access-Token") String token)  {
+        return ResponseEntity.ok(null);
+
+    }
+
+    @PutMapping("/{order-id}")
+    public ResponseEntity<OrderDto>updateOrder(@RequestBody UpdateOrderDto updateOrderDto,
+                                               @PathVariable("order-id") Integer orderId,
+                                               @RequestHeader("Access-Token") String token) {
+        return ResponseEntity.ok(null);
+    }
 }
+
--- src/main/java/pl/adrian/pizzaaplication/remote/rest/api/PizzaController.java
+++ src/main/java/pl/adrian/pizzaaplication/remote/rest/api/PizzaController.java
@@ -1,4 +1,34 @@
 package pl.adrian.pizzaaplication.remote.rest.api;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.adrian.pizzaaplication.remote.rest.dto.request.AddPizzaDto;
+import pl.adrian.pizzaaplication.remote.rest.dto.response.PizzaDto;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RequestMapping(value = "/api/v1/pizzas", produces = APPLICATION_JSON_VALUE)
+@RestController
 public class PizzaController {
+
+    @PostMapping
+    public ResponseEntity<PizzaDto> addPizza(@RequestHeader("Access-token") String token,
+                                             @RequestBody AddPizzaDto AddPizzaDto) {
+        return ResponseEntity.ok(null);
+    }
+
+    @PutMapping("/{pizza-id}")
+    public ResponseEntity<PizzaDto> updatePizza(@RequestBody PizzaDto PizzaDto,
+                                            @PathVariable("pizza-id") Integer orderId,
+                                            @RequestHeader("Access-Token") String token) {
+        return ResponseEntity.ok(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePizza(@PathVariable("id") Integer id,
+                                            @RequestHeader("Access-Token") String token)  {
+        return ResponseEntity.ok(null);
+    }
+
+
 }
