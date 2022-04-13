344e639dc3d5c060aebed13215d273859345fcf4
AdrianTm1 <awojciechowski1@tm1.edu.pl>
2022-04-13 13:24:45 +0200

    AddOrderService created

--- src/main/java/pl/adrian/pizzaaplication/domain/service/AddOrderService.java
+++ src/main/java/pl/adrian/pizzaaplication/domain/service/AddOrderService.java
@@ -1,15 +1,36 @@
 package pl.adrian.pizzaaplication.domain.service;
 
 import org.springframework.stereotype.Service;
+import pl.adrian.pizzaaplication.data.repository.SizeRepository;
+import pl.adrian.pizzaaplication.domain.exception.ResourceNotFoundException;
 import pl.adrian.pizzaaplication.remote.rest.dto.request.AddOrderDto;
 import pl.adrian.pizzaaplication.remote.rest.dto.response.TokenDto;
+
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class AddOrderService {
 
+    private final SizeRepository sizeRepository;
+
+    public AddOrderService(SizeRepository sizeRepository) {
+        this.sizeRepository = sizeRepository;
+    }
+
     public TokenDto addOrder(AddOrderDto addOrderDto) {
 
-        //stworzylismy tylko
+        Set<Integer> sizeIds = addOrderDto.getPizzas()
+                .stream()
+                .map(pizzaOrderDto -> pizzaOrderDto.getSizeId())
+                .collect(Collectors.toSet());
+        Boolean existSizes = sizeRepository.existsAllByIdIn(sizeIds);
+        if(!existSizes) {
+            throw new ResourceNotFoundException("Pizze o padanym rozmiarze nie istnieja w bazie danych");
+        }
+
+        String token = UUID.randomUUID().toString();
 
     }
 
--- src/main/java/pl/adrian/pizzaaplication/remote/rest/dto/request/AddOrderDto.java
+++ src/main/java/pl/adrian/pizzaaplication/remote/rest/dto/request/AddOrderDto.java
@@ -20,4 +20,20 @@ public class AddOrderDto {
         this.pizzas = pizzas;
         this.person = person;
     }
+
+    public List<PizzaOrderDto> getPizzas() {
+        return pizzas;
+    }
+
+    public void setPizzas(List<PizzaOrderDto> pizzas) {
+        this.pizzas = pizzas;
+    }
+
+    public PersonOrderDto getPerson() {
+        return person;
+    }
+
+    public void setPerson(PersonOrderDto person) {
+        this.person = person;
+    }
 }
