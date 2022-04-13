644043ff597a3556d49ead9c27b39c6f50bc9648
AdrianTm1 <awojciechowski1@tm1.edu.pl>
2022-04-13 13:10:53 +0200

    AddOrderService created(with nothing in it)

---
+++ src/main/java/pl/adrian/pizzaaplication/domain/service/AddOrderService.java
@@ -0,0 +1,16 @@
+package pl.adrian.pizzaaplication.domain.service;
+
+import org.springframework.stereotype.Service;
+import pl.adrian.pizzaaplication.remote.rest.dto.request.AddOrderDto;
+import pl.adrian.pizzaaplication.remote.rest.dto.response.TokenDto;
+
+@Service
+public class AddOrderService {
+
+    public TokenDto addOrder(AddOrderDto addOrderDto) {
+
+        //stworzylismy tylko
+
+    }
+
+}
